f66358a8bd347510c0dc6ae4b61a141558c46da8
Vitaly Orlov <pereplet@icloud.com>
2024-12-29 03:03:34 +0400

    RetroVibe Theme for Insomnia

---
+++ insomnia-plugin-retro-vibe/theme.js
@@ -0,0 +1,58 @@
+module.exports.themes = [
+    {
+      name: "retro-vibes",
+      displayName: "Retro Vibes",
+      theme: {
+        background: {
+          default: "#2b2b28",
+          success: "#a3d9a5",
+          notice: "#f4b860",
+          warning: "#f4976c",
+          danger: "#d65a31",
+          surprise: "#c9a9e0",
+          info: "#82c7f2"
+        },
+        foreground: {
+          default: "#e8e3d3",
+          success: "#2b2b28",
+          notice: "#2b2b28",
+          warning: "#2b2b28",
+          danger: "#2b2b28",
+          surprise: "#2b2b28",
+          info: "#2b2b28"
+        },
+        highlight: {
+          default: "#6c665f",
+          xsmall: "#9c8b8e",
+          small: "#ceb2a6",
+          medium: "#f4b860",
+          large: "#d65a31",
+          xlarge: "#82c7f2"
+        },
+        rawCss: "",
+        styles: {
+          sidebar: {
+            background: {
+              default: "#393530"
+            },
+            foreground: {
+              default: "#e8e3d3"
+            },
+            highlight: {
+              default: "#a3d9a5"
+            }
+          },
+          paneHeader: {
+            background: {
+              success: "#a3d9a5",
+              notice: "#f4b860",
+              warning: "#f4976c",
+              danger: "#d65a31",
+              surprise: "#c9a9e0",
+              info: "#82c7f2"
+            }
+          }
+        }
+      }
+    }
+  ];
